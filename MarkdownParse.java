8cb7b78adc035e7dc28b680cab650e853bd8d26f
Nicholas Chiu <cs15lsp22afz@ieng6-203.ucsd.edu>
2022-05-08 21:36:30 -0700

    Command line commit

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -50,5 +50,5 @@ public class MarkdownParse {
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
 	    System.out.println(links);
-    }
+    } 
 }
